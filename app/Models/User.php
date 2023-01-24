a4cfc9b1f9c174df800446e8d9cf0b8cfb494abe
abdelrahman <abdelrahman.magdy.gado@gmail.com>
2023-01-24 15:37:42 +0200

    create User Model

--- app/Models/User.php
+++ app/Models/User.php
@@ -12,6 +12,8 @@ class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
 
+    public const PAGINATION_COUNT = 5;
+
     /**
      * The attributes that are mass assignable.
      *
