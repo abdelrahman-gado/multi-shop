a03f4474523b6ce010443344dbc7d90a5494ccd6
abdelrahman <abdelrahman.magdy.gado@gmail.com>
2023-01-25 23:06:32 +0200

    create reviews migration

---
+++ database/migrations/2023_01_25_205720_create_reviews_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('reviews', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('product_id');
+            $table->unsignedBigInteger('user_id');
+            $table->text('reveiw');
+            $table->float('rating');
+            $table->timestamps();
+
+            $table->foreign('product_id')->references('id')->on('products')->cascadeOnDelete()->restrictOnUpdate();
+            $table->foreign('user_id')->references('id')->on('users')->cascadeOnDelete()->cascadeOnUpdate();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('reviews');
+    }
+};
