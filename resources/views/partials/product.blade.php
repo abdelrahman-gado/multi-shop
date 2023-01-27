43fad565d9d1198d04d3657a1197e6da855e24e7
abdelrahman <abdelrahman.magdy.gado@gmail.com>
2023-01-27 10:12:00 +0200

    remove first div to create general partial view that can be used with carsousel

--- resources/views/partials/product.blade.php
+++ resources/views/partials/product.blade.php
@@ -1,25 +1,23 @@
-<div class="col-lg-3 col-md-4 col-sm-6 pb-1">
-    <div class="product-item bg-light mb-4">
-        <div class="product-img position-relative overflow-hidden">
-            <img class="img-fluid w-100" src="{{ asset('storage/' . $product['image']) }}" alt="">
-            <div class="product-action">
-                <a class="btn btn-outline-dark btn-square" onclick="addProductToCart({{ $product['id'] }})"><i
-                        class="fa fa-shopping-cart"></i></a>
-                <a class="btn btn-outline-dark btn-square" onclick="addProductToLikedList({{ $product['id'] }})"><i
-                        class="far fa-heart"></i></a>
-            </div>
+<div class="product-item bg-light mb-4">
+    <div class="product-img position-relative overflow-hidden">
+        <img class="img-fluid w-100" src="{{ asset('storage/' . $product['image']) }}" alt="">
+        <div class="product-action">
+            <a class="btn btn-outline-dark btn-square" onclick="addProductToCart({{ $product['id'] }})"><i
+                    class="fa fa-shopping-cart"></i></a>
+            <a class="btn btn-outline-dark btn-square" onclick="addProductToLikedList({{ $product['id'] }})"><i
+                    class="far fa-heart"></i></a>
         </div>
-        <div class="text-center py-4">
-            <a class="h6 text-decoration-none text-truncate"
-                href="{{ url('/detail?id=' . $product['id']) }}">{{ $product['name'] }}</a>
-            <div class="d-flex align-items-center justify-content-center mt-2">
-                <h5>${{ $product->getPriceAfterDiscount() }}</h5>
-                <h6 class="text-muted ml-2"><del>${{ $product['price'] }}</del></h6>
-            </div>
-            @include('partials.stars', [
-                'rating' => $product['rating'],
-                'rating_count' => $product['rating_count'],
-            ])
+    </div>
+    <div class="text-center py-4">
+        <a class="h6 text-decoration-none text-truncate"
+            href="{{ url('/detail?id=' . $product['id']) }}">{{ $product['name'] }}</a>
+        <div class="d-flex align-items-center justify-content-center mt-2">
+            <h5>${{ $product->getPriceAfterDiscount() }}</h5>
+            <h6 class="text-muted ml-2"><del>${{ $product['price'] }}</del></h6>
         </div>
+        @include('partials.stars', [
+            'rating' => $product['rating'],
+            'rating_count' => $product['rating_count'],
+        ])
     </div>
 </div>
